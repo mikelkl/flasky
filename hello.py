eeb02e3fb1ad092b35acefde052b0c3567e94b98
Mike Liu <mikelkl@foxmail.com>
2016-02-14 19:40:31 +0800

    program & request context

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
 
 
 @app.route('/user/<name>')
