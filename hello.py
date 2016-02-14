9a5c8d17710ebc8e2d62fbfa1f2dc9db31bd2e2a
Mike Liu <mikelkl@foxmail.com>
2016-02-14 20:16:07 +0800

    render templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 
 app = Flask(__name__)
@@ -6,13 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s</p>' % user_agent
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
