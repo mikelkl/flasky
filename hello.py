d9258114814fdd412df5c54d36e942339745714a
Mike Liu <mikelkl@foxmail.com>
2016-02-14 21:03:28 +0800

    use Flask-Bootstrap

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-from flask import request
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
