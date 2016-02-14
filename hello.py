d414afde7aa979a059bae92b6043d75ede39bca5
Mike Liu <mikelkl@foxmail.com>
2016-02-14 18:15:33 +0800

    dynamic route

--- hello.py
+++ hello.py
@@ -8,5 +8,10 @@ def index():
     return '<h1>Hello World!</>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
 if __name__ == '__main__':
     app.run(debug=True)
