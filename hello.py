e62976a8d1b9def3e6b69fcb3a56f33dac884519
Mike Liu <mikelkl@foxmail.com>
2016-02-14 21:15:55 +0800

    Custom Error Pages

--- hello.py
+++ hello.py
@@ -15,5 +15,15 @@ def user(name):
     return render_template('user.html', name=name)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
