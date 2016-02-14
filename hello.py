7d973e00963798c5d03dbf5a7f0b9731dbd2ce31
Mike Liu <mikelkl@foxmail.com>
2016-02-14 21:55:09 +0800

    Dates and times with Flask-Moment

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
