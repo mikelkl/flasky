7cc72233ca3a12c545fb33d44da87533ed8fd02e
Mike Liu <mikelkl@foxmail.com>
2016-02-16 21:57:05 +0800

    define model

--- hello.py
+++ hello.py
@@ -25,6 +25,24 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
