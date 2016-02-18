804bb67495d3c51dca3b65efa4359b478fb5a76c
Mike Liu <mikelkl@foxmail.com>
2016-02-18 20:21:59 +0800

    Email support with Flask-Mail

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for
-from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
-from flask.ext.wtf import Form
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.moment import Moment
+from flask.ext.script import Manager, Shell
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
+from flask.ext.mail import Mail
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flask.ext.sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,6 +17,11 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -23,12 +29,14 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+mail = Mail(app)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
