2f58c1895052ab56ce8e8c5fa809587573e35c53
Mike Liu <mikelkl@foxmail.com>
2016-02-18 21:27:50 +0800

    使用程序工厂函数

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
+from config import config
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+mail = Mail()
+
+
+def create_db(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_name)
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # 附加路由和自定义的错误界面
+
+    return app
