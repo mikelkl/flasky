29607f97e7d15c20a2432f8dfafa3c2f0fcbfd85
Mike Liu <mikelkl@foxmail.com>
2016-02-19 20:06:42 +0800

    准备用于登录的用户模型

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask.ext.login import UserMixin
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -12,9 +12,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
@@ -31,3 +32,8 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
