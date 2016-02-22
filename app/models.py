ad09fe3a3e2e9c03f251dac812dbf7518f14b123
Mike Liu <mikelkl@foxmail.com>
2016-02-22 18:32:40 +0800

    Represent of roles in database

--- app/models.py
+++ app/models.py
@@ -9,7 +9,26 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permission = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICES, True),
+            'Moderator': (
+            Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICES | Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permission = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -95,3 +114,11 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
