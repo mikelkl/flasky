dfab98a72ca997886db2c700b37cbaa777e628dc
Mike Liu <mikelkl@foxmail.com>
2016-02-23 12:17:05 +0800

    User profile

--- app/auth/views.py
+++ app/auth/views.py
@@ -57,7 +57,7 @@ def confirm(token):
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated():
+    if current_user.is_authenticated:
         current_user.ping()
         if not current_user.confirmed and request.endpoint[:5] != 'auth.' and request.endpoint != 'static':
             return redirect(url_for('auth.unconfirmed'))
@@ -65,7 +65,7 @@ def before_request():
 
 @auth.route('/unconfirmed')
 def unconfirmed():
-    if current_user.is_anonymous() or current_user.confirmed:
+    if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
     return render_template('auth/unconfirmed.html')
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,18 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
 
+from flask import render_template, session, redirect, url_for, abort
 from . import main
 from .forms import NameForm
 from .. import db
 from ..models import User
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
 
 
 @main.route('/', methods=['GET', 'POST'])
