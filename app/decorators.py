b5c6f86d47ff91a9e991991bc1d05a1a93590a64
Mike Liu <mikelkl@foxmail.com>
2016-02-23 21:22:01 +0800

    User avatar

--- app/decorators.py
+++ app/decorators.py
@@ -12,8 +12,9 @@ def permission_required(permission):
             if not current_user.can(permission):
                 abort(403)
             return f(*args, **kwargs)
-
         return decorated_function
+
+    return decorator
 
 
 def admin_required(f):
--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
-from flask import current_app
+import hashlib
+from datetime import datetime
+
+from flask import current_app, request
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db, login_manager
-from datetime import datetime
+
 
 class Permission:
     FOLLOW = 0x01
@@ -11,6 +14,7 @@ class Permission:
     WRITE_ARTICLES = 0x04
     MODERATE_COMMENTS = 0x08
     ADMINISTER = 0x80
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -55,10 +59,18 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
-    def ping(self):
-        self.last_seen = datetime.utcnow()
-        db.session.add(self)
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+            if self.email is not None and self.avatar_hash is None:
+                self.avatar_hash = hashlib.md5(
+                    self.email.encode('utf-8')).hexdigest()
 
     @property
     def password(self):
@@ -121,27 +133,34 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
-
-    def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
-        if self.role is None:
-            if self.email == current_app.config['FLASKY_ADMIN']:
-                self.role = Role.query.filter_by(permissions=0xff).first()
-            if self.role is None:
-                self.role = Role.query.filter_by(default=True).first()
 
     def can(self, permissions):
         return self.role is not None and \
                (self.role.permissions & permissions) == permissions
 
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
 
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
     def __repr__(self):
         return '<User %r>' % self.username
-
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
