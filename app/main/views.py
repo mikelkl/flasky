fbfbdc43d4db6036e062c0aa712b13f8e5997369
Mike Liu <mikelkl@foxmail.com>
2016-02-23 12:44:29 +0800

    Profile editor with user level

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,28 @@
 from datetime import datetime
 
-from flask import render_template, session, redirect, url_for, abort
+from flask import render_template, session, redirect, url_for, abort, flash
+from flask.ext.login import login_required, current_user
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
 
 
 @main.route('/user/<username>')
