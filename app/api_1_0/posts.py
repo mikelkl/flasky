345e30691ee2ff084754902ecf2927b1beb7b2f0
Mike Liu <mikelkl@foxmail.com>
2016-02-26 15:36:36 +0800

    分页大型资源集合

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -1,21 +1,33 @@
-from flask import jsonify, request, g, url_for
+from flask import jsonify, request, g, abort, url_for, current_app
+from .. import db
+from ..models import Post, Permission
 from . import api
 from .decorators import permission_required
-from .. import db
-from ..auth import auth
-from ..models import Post, Permission
 from .errors import forbidden
 
 
 @api.route('/posts/')
-@auth.login_required
 def get_posts():
-    posts = Post.query.all()
-    return jsonify({'posts': [post.to_json() for post in posts]})
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page - 1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page + 1, _external=True)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
 
 
 @api.route('/posts/<int:id>')
-@auth.login_required
 def get_post(id):
     post = Post.query.get_or_404(id)
     return jsonify(post.to_json())
